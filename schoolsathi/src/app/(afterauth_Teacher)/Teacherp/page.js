e1513699d289d90e41e04a329f0cdeeea05e1a55
Mishan Adhikari <mishanadhikari8@gmail.com>
2024-07-16 16:57:43 +0545

    changes

---
+++ schoolsathi/src/app/(afterauth_Teacher)/Teacherp/page.js
@@ -0,0 +1,53 @@
+'use client'
+import React from "react";
+import {Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Input, DropdownItem, DropdownTrigger, Dropdown, DropdownMenu, Avatar} from "@nextui-org/react";
+export default function teacher() {
+  return (
+    <div className="min-h-screen bg-white">
+    <Navbar isBordered>
+      <NavbarContent justify="start">
+        <NavbarBrand className="mr-4">
+          <p className="font-bold text-black text-2xl">SchoolSathi</p>
+        </NavbarBrand>
+        <NavbarContent className="hidden sm:flex gap-3">
+          <NavbarItem>
+          <Input type="search" label="search" variant="bordered"  className=" h-6 mb-3 w-64" />
+          </NavbarItem>
+        </NavbarContent>
+      </NavbarContent>
+
+      <NavbarContent as="div" className="items-center" justify="end">
+     
+        <Dropdown placement="bottom-end">
+          <DropdownTrigger>
+            <Avatar
+              isBordered
+              as="button"
+              className="transition-transform"
+              color="secondary"
+              name="Jason Hughes"
+              size="sm"
+              src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
+            />
+          </DropdownTrigger>
+          <DropdownMenu aria-label="Profile Actions" variant="flat">
+            <DropdownItem key="profile" className="h-14 gap-2 text-black">
+              <p className="font-semibold">Signed in as</p>
+              <p className="font-semibold">zoey@example.com</p>
+            </DropdownItem>
+            <DropdownItem key="settings" className="text-black">My Settings</DropdownItem>
+            <DropdownItem className="text-black" key="team_settings">Team Settings</DropdownItem>
+            <DropdownItem className="text-black" key="analytics">Analytics</DropdownItem>
+            <DropdownItem className="text-black" key="system">System</DropdownItem>
+            <DropdownItem className="text-black" key="configurations">Configurations</DropdownItem>
+            <DropdownItem className="text-black" key="help_and_feedback">Help & Feedback</DropdownItem>
+            <DropdownItem className="text-black" key="logout" color="danger">
+              Log Out
+            </DropdownItem>
+          </DropdownMenu>
+        </Dropdown>
+      </NavbarContent>
+    </Navbar>
+    </div>
+  );
+}
---
+++ schoolsathi/src/app/(auth)/register/page.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import {Card, Button,Input, CardHeader, CardBody, CardFooter, Divider, Link, Image} from "@nextui-org/react";
+
+const register = () => {
+  return (
+<div className=" min-h-screen bg-white text-center">
+      <h1 className="text-gray-700 text-4xl font-bold relative top-32">Welcome, Create your School account</h1>
+      <p className="text-gray-700 relative top-44 text-xl">It is our great pleasure to have you on board</p>
+    <Card className="max-w-[400px] mx-auto overflow-hidden mt-52 w-[700px]">
+      <CardBody className="p-3 ">
+        <Input type="text" placeholder="Enter the name of the admin" className="m-2 w-[50]"/>
+        <Input type="text" placeholder="Enter the name of school" className="m-2 w-[50]"/>
+        <Input type="text" placeholder="Enter the school email" className="m-2 w-[50]"/>
+        <Input type="password" varient="underlined" placeholder="Enter your password" className="m-2 w-[50]"/>
+        <Input type="password" varient="underlined" placeholder="Confirm password" className="m-2 w-[50]"/>
+        <Button color="primary"> Signup</Button>
+      </CardBody>
+      <CardFooter>
+        <p>Already have an account? </p>
+        <Link
+          isExternal
+          href="https://github.com/nextui-org/nextui"
+        >
+          Signup
+        </Link>
+      </CardFooter>
+    </Card>
+    </div>
+  )
+}
+
+export default register
